254f5ceee229bff990637ff28fb9c41624c9cd34
Christian Ceciliano <cristiancehi@gmail.com>
2014-05-13 18:39:21 -0500

    Correcciones en el mapa Actualizaciones en /usuario

--- static/js/supervisionDataFormat.js
+++ static/js/supervisionDataFormat.js
@@ -17,19 +17,22 @@ function innerDataFormat (element, lastLetter, query, reg, usr) {
 		var tdMargen = td(data.activity[i].margen_error);
 		var tdLat = td(data.activity[i].lat);
 		var tdLng = td(data.activity[i].lng);
-		var tdFecha = td(Date(data.activity[i].fecha).toString());
+		var tdFecha = td(new Date(data.activity[i].fecha).toString());
 		var tdSitio = td(data.activity[i].sitio__nombre);
 		var tdMapa = td("Mapa");
 		tdMapa.setAttribute("id", "showMap");
 		tdMapa.setAttribute("name", "showMap");
-		var mapaFunct = function(lat, lng){
+		var mapaFunct = function(lat, lng, name, fecha){
 			this.mapa = function(){
+				initialize();
 				$( "#map_container" ).dialog( "open" );
-				map.setCenter(new google.maps.LatLng(lat, lng), 10);
+				map.setCenter(new google.maps.LatLng(lat, lng));
+				map.setZoom(20);
+				plotPoint(lat,lng,name,'<span class="gBubble"><b>'+(new Date(fecha)).toString()+'</b></span>');
 				return false;
 			};
 		}
-		var mapa = (new mapaFunct(data.activity[i].lat, data.activity[i].lng)).mapa;
+		var mapa = (new mapaFunct(data.activity[i].lat, data.activity[i].lng, data.activity[i].sitio__nombre, (new Date(data.activity[i].fecha)).toString())).mapa;
 		tdMapa.onclick = mapa;
 		tr.appendChild(tdTipo);
 		tr.appendChild(tdMargen);
@@ -45,7 +48,23 @@ function innerDataFormat (element, lastLetter, query, reg, usr) {
 
 $(document).ready(function(){
 	$("#actividadTemplate").hide();
-	$("#usuario #t1 td").val(data.first_name + " " + data.last_name);
-	$("#usuario #t3 td.usuariodatos").val(data.provider);
-	$("#usuario #t4 td.usuariodatos").val(data.phone);
+	var remplazarTexto = function(query, texto){
+		return remplazar(query, document.createTextNode(texto));
+	}
+	var remplazar = function(query, node){
+		var parentNode = document.querySelector(query);
+		var childs = parentNode.childNodes;
+		for (var i = childs.length - 1; i >= 0; i--) {
+			parentNode.removeChild(childs[i]);
+		};
+		parentNode.appendChild(node);
+		return parentNode
+	}
+	remplazarTexto("#usuario #t1 td", data.first_name + " " + data.last_name);
+	var ultimotd = document.createTextNode("Ultimo Registro");
+	var td = document.createElement("td");
+
+	// remplazarTexto("#usuario #t2 td.usuariodatos");
+	remplazarTexto("#usuario #t3 td.usuariodatos", data.provider);
+	remplazarTexto("#usuario #t4 td.usuariodatos", data.phone);
 });
