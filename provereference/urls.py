c6c5283bcd785649ba9cfb8dcc1a66794da86b22
solorulo <solorulo@hotmail.com>
2014-05-13 15:37:36 -0500

    settings

--- provereference/urls.py
+++ provereference/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
