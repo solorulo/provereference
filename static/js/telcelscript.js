2ac0b9a5d2481d391d565c0ca96813cb53688f05
Christian Ceciliano <cristiancehi@gmail.com>
2014-06-24 12:35:02 -0500

    Bug fixed: Compañía (guardar/cancelar)

--- static/js/telcelscript.js
+++ static/js/telcelscript.js
@@ -457,9 +457,9 @@ function companie(i, id){
 		abrir0();
 		document.querySelector("#popup1 .form_nombre").value = data.companies[i].name;
 		function searchRegion(){
-			for (var i = 0; i < data.regiones.length; i++) {
-				if ("Región "+data.regiones[i].nombre == data.companies[i].reg){
-					return i+1;
+			for (var e = 0; e < data.regiones.length; e++) {
+				if ("Región "+data.regiones[e].nombre == data.companies[i].reg){
+					return e+1;
 				}
 			}
 		}
